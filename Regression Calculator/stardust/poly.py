acf96df73ae56a8f9e0da897f521c489c107a2d5
StardustGogeta <liam.gerst@gmail.com>
2017-02-02 21:28:46 -0500

    Matrix and polynomial fit improvements V2 (#11)
    * Rewrite matrix.py
    
    Also, fixing some spacing issues and adding explanatory comments.
    
    * Replacing polynomial fit
    
    This now uses a simple, built-in numpy function instead of matrix operations.

--- Regression Calculator/stardust/poly.py
+++ Regression Calculator/stardust/poly.py
@@ -1,15 +1,15 @@
+NUMPY = 0
 try:
-    import numpy as np
+    from numpy import polyfit
+    NUMPY = 1
 except:
     import matrix
 
 def poly(x,y):
-    a = [[a**b for b in range(len(x)-1,-1,-1)] for a in x]
-    b = [[t] for t in y]
-    try:
-        a,b = np.matrix(a),np.matrix(b)
-        c = a.I*b
-        c = [str(round(c[X].A1[0],10)) for X in range(len(x))]
-    except ValueError:
+    if NUMPY:
+        c = [str(x) for x in polyfit(x,y,len(x)-1)]
+    else:
+        a = [[a**b for b in range(len(x)-1,-1,-1)] for a in x]
+        b = [[t] for t in y]
         c = [str(round(A,14)) for B in matrix.mult(matrix.inv(a),b) for A in B]
     return ' + '.join([X for X in (c[A]+'x'+''.join([['⁰','¹','²','³','⁴','⁵','⁶','⁷','⁸','⁹'][int(y)] for y in str(len(c)-int(A)-1)]) if float(c[A])!=0 else '' for A in range(len(c))) if X])
