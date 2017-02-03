71f86fc5a707518b7f61940fe19b0e7d9f4104ad
StardustGogeta <liam.gerst@gmail.com>
2017-02-02 21:38:28 -0500

    Standardizing spacing and fixing formulae (#12)
    * Tidying up listf.py and oldmath.py
    
    This identical commit was destroyed when the files were moved, so I have copied this to the new position. This contains spacing and optimization improvements.

--- Regression Calculator/cat/oldmath.py
+++ Regression Calculator/cat/oldmath.py
@@ -1,36 +1,24 @@
-'''
-class for math computations
-'''
+# Class for math computations
 
-'''
-Determines number of decimal places a number has
-'''
+# Determines number of decimal places a number has.
 def decimalplaces(num):
     return len(str(round(num%1,15)))
 
-'''
-takes the root of a number with a given base
-Returns numerical value
-Use newmath.root() for display strings
-'''
+# Takes the root of a number with a given base.
+# Returns numerical value.
+# Use newmath.root() for display strings.
 def root(n, base=2):
     return n**(1.0/base)
 
-'''
-Returns true if input is integer, false if not
-'''
+# Returns true if input is integer, false if not.
 def isint(num):
     return num%1==0
 
-'''
-returns true of input is close to an integer (within defined range)
-'''
+# Returns true of input is close to an integer (within defined range).
 def closeint(num, r=.05):
     return abs(round(num)-num)<=r 
 
-'''
-compares to list of known fractions
-returns true if in it
-'''
+# Compares to list of known fractions.
+# Returns true if in it.
 def isfrac(num, fracs=[float(x)/y for y in [9,16] for x in range(1,y)]):
     return num in fracs
